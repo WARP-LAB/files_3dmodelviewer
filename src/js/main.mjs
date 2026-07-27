30949d20a612af4cc4d5041bcbf23d1adfba047a
kroko <720976+kroko@users.noreply.github.com>
2026-07-27 16:11:14 +0300

    catch silently

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -9,31 +9,38 @@ import {
 import App from 'App/App.vue'; // eslint-disable-line import/no-unresolved, n/no-missing-import
 
 if (OCA.Viewer) {
-  OCA.Viewer.registerHandler({
-    id: APP_ID,
-    group: '3d',
-    mimes: [
-      'model/3dm-nospec',
-      'model/3ds-nospec',
-      'model/3mf',
-      'model/amf',
-      'model/bim-nospec',
-      'model/brep-nospec',
-      'model/vnd.collada+xml',
-      'model/fbx-nospec',
-      'model/fcstd-nospec',
-      'model/gltf-binary',
-      'model/gltf+json',
-      'application/x-step',
-      'model/iges',
-      'model/obj',
-      'model/off-nospec',
-      'model/ply-nospec', 'model/vnd.ply',
-      'model/step',
-      'model/stl', 'application/sla',
-      'model/vrml',
-      // TODO: 'text/x-gcode',
-    ],
-    component: App,
-  });
+  try {
+    OCA.Viewer.registerHandler({
+      id: APP_ID,
+      group: '3d',
+      canCompare: false,
+      mimes: [
+        'model/3dm-nospec',
+        'model/3ds-nospec',
+        'model/3mf',
+        'model/amf',
+        'model/bim-nospec',
+        'model/brep-nospec',
+        'model/vnd.collada+xml',
+        'model/fbx-nospec',
+        'model/fcstd-nospec',
+        'model/gltf-binary',
+        'model/gltf+json',
+        'application/x-step',
+        'model/iges',
+        'model/obj',
+        'model/off-nospec',
+        'model/ply-nospec', 'model/vnd.ply',
+        'model/step',
+        'model/stl', 'application/sla',
+        'model/vrml',
+        // TODO: 'text/x-gcode',
+      ],
+      component: App,
+    });
+  } catch (error) {
+    if (error?.message?.includes('already registered') || error?.message?.includes('duplicate') || error?.message?.includes('same name')) {} else {
+      console.error('error in registerHandler:', error);
+    }
+  }
 }
